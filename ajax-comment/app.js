129f26ab0cd06d137683d2123eae361fc7854402
tabyouto <tabyouto@gmail.com>
2016-07-23 11:17:22 +0800

    add js validate

--- ajax-comment/app.js
+++ ajax-comment/app.js
@@ -2,52 +2,66 @@ jQuery(document).ready(function(jQuery) {
 	var __cancel = jQuery('#cancel-comment-reply-link'),
 		__cancel_text = __cancel.text(),
 		__list = 'comment-list';//your comment wrapprer
+	var author = $('#author'),
+        email = $('#email'),
+        comment = $('#comment');
+    var re = /[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/igm;
 	jQuery(document).on("submit", "#commentform", function() {
-		jQuery.ajax({
-			url: ajaxcomment.ajax_url,
-			data: jQuery(this).serialize() + "&action=ajax_comment",
-			type: jQuery(this).attr('method'),
-			beforeSend: addComment.createButterbar("提交中...."),
-			error: function(request) {
-				var t = addComment;
-				t.createButterbar(request.responseText);
-			},
-			success: function(data) {
-				jQuery('textarea').each(function() {
-					this.value = ''
-				});
-				var t = addComment,
-					cancel = t.I('cancel-comment-reply-link'),
-					temp = t.I('wp-temp-form-div'),
-					respond = t.I(t.respondId),
-					post = t.I('comment_post_ID').value,
-					parent = t.I('comment_parent').value;
-				if (parent != '0') {
-					jQuery('#respond').before('<ol class="children">' + data + '</ol>');
-				} else if (!jQuery('.' + __list ).length) {
-					if (ajaxcomment.formpostion == 'bottom') {
-						jQuery('#respond').before('<ol class="' + __list + '">' + data + '</ol>');
-					} else {
-						jQuery('#respond').after('<ol class="' + __list + '">' + data + '</ol>');
-					}
+	    var t = addComment;
+	    if( author.val() && email.val() && comment.val()) {
+	        if(re.test(email.val())) {
+                jQuery.ajax({
+                url: ajaxcomment.ajax_url,
+                data: jQuery(this).serialize() + "&action=ajax_comment",
+                type: jQuery(this).attr('method'),
+                beforeSend: addComment.createButterbar("提交中...."),
+                error: function(request) {
+                    var t = addComment;
+                    t.createButterbar(request.responseText);
+                },
+                success: function(data) {
+                    jQuery('textarea').each(function() {
+                        this.value = ''
+                    });
+                    var t = addComment,
+                        cancel = t.I('cancel-comment-reply-link'),
+                        temp = t.I('wp-temp-form-div'),
+                        respond = t.I(t.respondId),
+                        post = t.I('comment_post_ID').value,
+                        parent = t.I('comment_parent').value;
+                    if (parent != '0') {
+                        jQuery('#respond').before('<ol class="children">' + data + '</ol>');
+                    } else if (!jQuery('.' + __list ).length) {
+                        if (ajaxcomment.formpostion == 'bottom') {
+                            jQuery('#respond').before('<ol class="' + __list + '">' + data + '</ol>');
+                        } else {
+                            jQuery('#respond').after('<ol class="' + __list + '">' + data + '</ol>');
+                        }
 
-				} else {
-					if (ajaxcomment.order == 'asc') {
-						jQuery('.' + __list ).append(data); // your comments wrapper
-					} else {
-						jQuery('.' + __list ).prepend(data); // your comments wrapper
-					}
-				}
-				t.createButterbar("提交成功");
-				cancel.style.display = 'none';
-				cancel.onclick = null;
-				t.I('comment_parent').value = '0';
-				if (temp && respond) {
-					temp.parentNode.insertBefore(respond, temp);
-					temp.parentNode.removeChild(temp)
-				}
-			}
-		});
+                    } else {
+                        if (ajaxcomment.order == 'asc') {
+                            jQuery('.' + __list ).append(data); // your comments wrapper
+                        } else {
+                            jQuery('.' + __list ).prepend(data); // your comments wrapper
+                        }
+                    }
+                    t.createButterbar("提交成功");
+                    cancel.style.display = 'none';
+                    cancel.onclick = null;
+                    t.I('comment_parent').value = '0';
+                    if (temp && respond) {
+                        temp.parentNode.insertBefore(respond, temp);
+                        temp.parentNode.removeChild(temp)
+                    }
+                }
+            });
+            }else {
+                t.createButterbar('邮箱错啦！（･∀･)');
+            }
+        }else {
+
+            t.createButterbar('是不是漏写了什么？（･∀･)');
+        }
 		return false;
 	});
 	addComment = {
